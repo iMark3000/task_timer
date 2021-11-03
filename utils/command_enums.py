c47372d4d475cec051764dc2003913ea9dd9b7bc
iMark3000 <markyoung.ca@gmail.com>
2021-11-03 10:22:42 -0700

    Added DEACTIVATE, REACTIVAT, SWITCH to InputTypes

--- utils/command_enums.py
+++ utils/command_enums.py
@@ -8,9 +8,12 @@ class InputType(Enum):
     STOP = auto()   # Stop (Ends) Session
     STATUS = auto()  # If session in prog, gets status
     NEW = auto()    # Creates new project
-    PROJECTS = auto()   # Gets a list of projects and their IDS
+    PROJECTS = auto()   # Gets a list of projects and their IDS -> add optional arg to filter by name
     FETCH = auto()  # Fetches a project by an ID number to start session with
-    MERGE = auto()
+    DEACTIVATE = auto()  # Deactivates projects
+    REACTIVATE = auto()  # Reactivates projects that been deactivated
+    SWITCH = auto()  # Switches Queued Project...to use when multiples sessions feature is implemented
+    MERGE = auto()  # Merges two projects (their sessions and logs) into one project
     LOG = auto()
     REPORT = auto()
     NO_SESSION = auto() # Placeholder value for when no session is in progress
