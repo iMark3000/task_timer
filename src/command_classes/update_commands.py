270ef22efdd1c0b7b3a773c9ca2ccf92c5402f86
Mark <markyoung.ca@gmail.com>
2022-03-11 12:08:37 -0800

    Added doc strings to update_commands

--- src/command_classes/update_commands.py
+++ src/command_classes/update_commands.py
@@ -6,10 +6,12 @@ class UpdateCommand(CommandBaseClass):
 
 
 class DeactivateCommand(UpdateCommand):
+    """Deactivates project by project ID"""
     pass
 
 
 class ReactivateCommand(UpdateCommand):
+    """Reactivates project by project ID"""
     pass
 
 
@@ -18,8 +20,19 @@ class EditCommand(UpdateCommand):
 
 
 class MergeCommand(UpdateCommand):
+    """Merges two or more projects into one. Optional argument 'name'
+
+    Input list of two or more project ids separated by spaces. If the optional 'name' argument is not provided, then
+    the first project listed will absorb all of the sessions and logs of the other projects listed. Those projects
+    will then be deactivated.
+
+    If the 'name' argument is included, then a new project is created and all the sessions and logs of the listed
+    projects will be moved to that new project. All listed projects will be deactivated.
+
+    """
     pass
 
 
 class RenameCommand(UpdateCommand):
+    """Takes in a project id and 'name' argument to rename a project"""
     pass
